b9419de9154436020d1e04a0f81ab1907b48818d
Thiago Oliveira <72051208+Thiolivs@users.noreply.github.com>
2023-11-09 01:44:50 -0300

    Create 1035.js

---
+++ JavaScript/1035.js
@@ -0,0 +1,12 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+    
+const space = ' '
+const [A, B, C, D] = lines[0].split(space)
+
+if ( B>C && D>A && C+D> A+B && C>=0 && D>=0 && A%2===0 ) {
+    console.log ("Valores aceitos")
+    }else {
+        console.log ("Valores nao aceitos")
+
+    }
