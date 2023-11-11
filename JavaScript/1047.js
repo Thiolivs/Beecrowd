3541b0b562a259b9f7e2b3c5648b69ee721a3fc2
Thiago Oliveira <72051208+Thiolivs@users.noreply.github.com>
2023-11-11 01:04:49 -0300

    Create 1047.js

---
+++ JavaScript/1047.js
@@ -0,0 +1,37 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+var line = lines.shift().split(" ");
+
+let hoursInitial = parseInt(line[0]);
+let minuteInitial = parseInt(line[1]);
+let hoursFinal = parseInt(line[2]);
+let minuteFinal = parseInt(line[3]);
+
+let minutes = 0;
+let hours = 0;
+
+if (
+  hoursInitial == hoursFinal &&
+  minuteInitial == minuteFinal &&
+  hoursInitial == minuteFinal
+) {
+  console.log("O JOGO DUROU 24 HORA(S) E 0 MINUTO(S)");
+} else {
+  if (hoursInitial > hoursFinal) {
+    hours = 24 - hoursInitial + hoursFinal;
+  } else if (hoursInitial < hoursFinal) {
+    hours = 24 - (24 - hoursFinal + hoursInitial);
+  }
+  if (minuteInitial > minuteFinal) {
+    minutes = 60 + minuteFinal - minuteInitial;
+    if (hoursInitial == hoursFinal) {
+      hours = 24 + hours - 1;
+    } else {
+      hours = hours - 1;
+    }
+  } else {
+    minutes = minuteFinal - minuteInitial;
+  }
+  console.log(`O JOGO DUROU ${hours} HORA(S) E ${minutes} MINUTO(S)`);
+}
